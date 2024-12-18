ed257afc1e6bfc0b0267d9606e9649d71fa37be6
douglasmadeiro1 <135288383+douglasmadeiro1@users.noreply.github.com>
2024-12-18 19:03:16 -0300

    change color
    change color

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3223979087"
+    serviceWorkerVersion: "3160121673"
   }
 });
